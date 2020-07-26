b61606feeca777f31c85aa373fa4d2f2b1c46baa
hayashi eijiro <ejrhys@gmail.com>
2020-07-26 15:12:20 +0900

    `from home on mac 20200726`

--- moviedb/urls.py
+++ moviedb/urls.py
@@ -6,3 +6,9 @@ app_name = 'moviedb'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
 ]
+
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path('detail/<int:pk>', views.MovieDetail.as_view(), name='movie_detail'),
+    path('list/', views.Movielist.as_view(), name='movie_list'),
+]
