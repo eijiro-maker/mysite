af78897e61110db2fe7fdf93ed736a887be185f0
hayashi eijiro <ejrhys@gmail.com>
2020-08-24 07:53:46 +0900

    `from home on mac 20200726-3`db更新、リスト作成

--- moviedb/views.py
+++ moviedb/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Movie
+from django.db.models import Q
 
 # Create your views here.
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
+    model = Movie
     template_name = "moviedb/index.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        q_word = self.request.GET.get('query')
+
+        if q_word:
+            object_list = Movie.objects.filter(
+                Q(title__icontains=q_word) | Q(explain__icontains=q_word))
+        else:
+            object_list = Movie.objects.all()
+        return object_list
 
 class MovieDetail(generic.DetailView):
     model = Movie
 
 class Movielist(generic.ListView):
     model = Movie
+    template_name = 'moviedb/index.html'
+    paginate_by = 10
