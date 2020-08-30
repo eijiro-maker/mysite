ab2b338ce56264591e76951e85788b2c9a754f47
hayashi eijiro <ejrhys@gmail.com>
2020-08-30 16:46:33 +0900

    `from home on mac 20200726-3`db更新、リスト作成

--- moviedb/views.py
+++ moviedb/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Movie
 from django.db.models import Q
+from moviedb.forms import SearchForm
+from django.utils import timezone
 
 # Create your views here.
 
@@ -9,6 +11,7 @@ class IndexView(generic.ListView):
     model = Movie
     template_name = "moviedb/index.html"
     paginate_by = 10
+    f = SearchForm()
 
     def get_queryset(self):
         q_word = self.request.GET.get('query')
@@ -18,12 +21,33 @@ class IndexView(generic.ListView):
                 Q(title__icontains=q_word) | Q(explain__icontains=q_word))
         else:
             object_list = Movie.objects.all()
+        RNum = object_list.count
         return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        context['Qstring'] = self.request.GET.get('query')
+        context['qword'] = self.request.GET.urlencode()
+        context['Num'] = self.object_list.count()
+        return context
 
 class MovieDetail(generic.DetailView):
     model = Movie
 
 class Movielist(generic.ListView):
     model = Movie
-    template_name = 'moviedb/index.html'
+    template_name = 'moviedb/movie_list.html'
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+    def get_queryset(self):
+        # original qs
+        qs = super().get_queryset()
+        # filter by a variable captured from url, for example
+        #return qs.filter(explain__startswith=self.kwargs['name'])
+        return qs.filter(explain__icontains='アカデミー')
